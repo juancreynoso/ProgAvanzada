fde17ee0be5b914c5a1d98784f524a5c4b4601ee
juancreynoso <juancreynoso11@gmail.com>
2023-04-04 19:35:51 -0300

    Cambios depto agu

--- practica3.hs
+++ practica3.hs
@@ -29,11 +29,30 @@ binarioPar n
             | otherwise = True
 
 --Ejercicio 6
-
+distanciaH :: Eq a => [a] -> [a] -> Int
+distanciaH [] [] = 0
+distanciaH xs [] = 0
+distanciaH [] ys = 0
+distanciaH (x:xs) (y:ys) 
+                        | x /= y = 1 + distanciaH xs ys
+                        | otherwise = 0 + distanciaH xs ys
 
 --Ejercicio 7
 cuadPerf :: Int -> [Int]
 cuadPerf n = [x | x <- [0..n], x*x == n]
 
+--Ejercicio 8
+repetidos ::  a -> Int -> [a]
+repetidos x 0 = []
+repetidos x 1 = [x]
+repetidos x n = x : repetidos x (n-1)
+
+--Ejercicio 10
+posicionesC :: [Char] -> Char -> Int -> [Int]
+posicionesC [] c pos = []
+posicionesC (x:xs) c pos
+                        | x /= c = posicionesC xs c (pos+1)
+                        | otherwise = pos : posicionesC xs c (pos+1)
+                    
 
 
