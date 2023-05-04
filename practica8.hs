54bac549b2853f16ec7b23de7ba101cec8ed1ae2
juancreynoso <juancreynoso11@gmail.com>
2023-05-04 09:40:50 -0300

    Actividad 2 realizada

--- practica8.hs
+++ practica8.hs
@@ -2,3 +2,11 @@ nand :: Bool -> Bool -> Bool
 nand True True = False
 nand True False = True
 nand False p = True
+
+maj :: Bool -> Bool -> Bool -> Bool
+maj True True _ = True
+maj _ True True = True
+maj True _ True = True
+maj False False _ = False
+maj False _ False = False
+maj _ False False = False
