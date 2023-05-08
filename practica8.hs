849ef3eebfa77d08e58a2c185b16713b01c7a2c8
juancreynoso <juancreynoso11@gmail.com>
2023-05-08 18:21:21 -0300

    practica 8 para todo

--- practica8.hs
+++ practica8.hs
@@ -1,8 +1,12 @@
+
+-- Ejercicio 1
 nand :: Bool -> Bool -> Bool
 nand True True = False
 nand True False = True
 nand False p = True
 
+
+-- Ejercicio 2
 maj :: Bool -> Bool -> Bool -> Bool
 maj True True _ = True
 maj _ True True = True
@@ -10,3 +14,7 @@ maj True _ True = True
 maj False False _ = False
 maj False _ False = False
 maj _ False False = False
+
+-- Ejercicio 3
+paraTodo :: [Int] -> [a] -> (Int -> [a] -> Bool)-> Bool
+paraTodo xs ys 
